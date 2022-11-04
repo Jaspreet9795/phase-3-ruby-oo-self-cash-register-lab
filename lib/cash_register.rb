1085a3bf31a79e7fb2b9b09450a7317a207b17d4
Jaspreet Kaur <kaurjaspreet9795@gmail.com>
2022-11-04 13:42:50 -0700

    commit tree f57a0065f157d2a94253793be5012be2df56e4cc

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+    attr_accessor  :discount, :total
 
+    def initialize(discount)
+        @discount=discount
+        @total = 0
+    end
+
+    def add_item(title, price, quantity=1)
+       self.total += price
+    end
+
+#    def apply_discount
+#     if self.discount !=0
+
+end 
